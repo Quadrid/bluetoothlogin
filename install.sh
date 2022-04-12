c7f395c7c199b2439e622a2b680fcc4b712a6dc3
lorhof1 <61967687+lorhof1@users.noreply.github.com>
2022-04-12 17:35:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-sudo cp src/usblogin /bin/usblogin
-sudo chmod +x /bin/usblogin
-sudo usblogin init
+sudo cp src/bluetoothlogin /bin/bluetoothlogin
+sudo chmod +x /bin/bluetoothlogin
+sudo bluetoothlogin init
